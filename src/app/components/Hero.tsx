e69184a8c34e8c617f5e43f5d746a662a95d8243
YavieAzka <yavieazkaputra@gmail.com>
2025-09-02 20:21:16 +0700

    fix: error deployment

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import { delay, motion } from "framer-motion";
+import { motion } from "framer-motion";
 import { useCursorContext } from "@/context/CursorContext";
 
 interface Greeting {
